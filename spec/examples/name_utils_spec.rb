afae0606ea5e7685bb7b764e519c37a986a6db30
cezaralmeidajr <cezar.almeidajr@gmail.com>
2015-12-01 22:43:06 -0200

    readme improvements

--- spec/examples/name_utils_spec.rb
+++ spec/examples/name_utils_spec.rb
@@ -3,37 +3,37 @@ require 'support/database_connection'
 
 Spec::Support::DatabaseConnection.establish_sqlite_connection
 
-# the abstraction:
-
-module NameUtils
-  def self.map_all_names
-    to_a.map &:name
+module NameUtils extend ActiveSupport::Concern
+  included do
+    define_singleton_method :map_all_names do
+      all.map &:name
+    end
   end
 end
 
 describe NameUtils do
 
   before do
-    create_dogs_model
-    populate_dogs_table
+    create_mappable_names_model
+    populate_mappable_names_table
   end
 
   after { ModelBuilder.clean }
 
-  it do
-    puts 'hi'
-  end
+  subject { MappableName.map_all_names }
 
-  def create_dogs_model
-    ModelBuilder.build 'Dog', {
-      includes: [NameUtils],
+  it { is_expected.to match %w(Name0 Name1 Name2) }
+
+  def create_mappable_names_model
+    ModelBuilder.build 'MappableName', {
+      includes: NameUtils,
       attributes: { name: :string },
       validates: [:name, presence: true]
     }
   end
 
-  def populate_dogs_table
-    3.times {|i| Dog.create! name: "Dog #{i}" }
+  def populate_mappable_names_table
+    3.times {|i| MappableName.create! name: "Name#{i}" }
   end
 
 end
